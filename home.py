7a109f3e53445b87ddddc3b02439115811c858f5
Ddhruv-IOT <ddhruvarora2@gmail.com>
2023-02-25 13:28:33 +0530

    home page file

---
+++ home.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 25 10:53:01 2023
+
+@author: ACER
+"""
+
+from utils import md_runner
+
+def home_page():
+    md_runner("""<h1>Hi There <img src="https://raw.githubusercontent.com/Ddhruv-IOT/Ddhruv-IOT/main/assetes/wave-hello.gif" height=80px width=80px/></h1>""")
